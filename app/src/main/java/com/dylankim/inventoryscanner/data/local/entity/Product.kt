66394d0ff4aeac55df662cc56bd3f3fe4e350814
Dylan Kim <dylan2.1kim@gmail.com>
2026-08-24 14:12:42 +0900

    feat: add Product entity

---
+++ app/src/main/java/com/dylankim/inventoryscanner/data/local/entity/Product.kt
@@ -0,0 +1,28 @@
+package com.dylankim.inventoryscanner.data.local.entity
+
+import androidx.room3.Entity
+import androidx.room3.ForeignKey
+import androidx.room3.Index
+import androidx.room3.PrimaryKey
+
+@Entity(tableName = "Product",
+        indices = [
+            Index(value = ["barcode"], unique = true)
+        ],
+        foreignKeys = [
+            ForeignKey(
+                entity = Company::class,
+                parentColumns = ["id"],
+                childColumns = ["companyId"]
+            )
+        ]
+)
+data class Product(
+    @PrimaryKey(autoGenerate = true)
+    val id:Long = 0,
+    val companyId:Long,
+    val productCode:String,
+    val barcode:String,
+    val name:String,
+    val price:String
+)
