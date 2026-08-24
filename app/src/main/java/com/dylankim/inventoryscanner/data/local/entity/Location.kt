92c1fdcf7f157cc0bebf3407d16cf094b63215be
Dylan Kim <dylan2.1kim@gmail.com>
2026-08-24 15:58:06 +0900

    feat: add inventory database entities

--- app/src/main/java/com/dylankim/inventoryscanner/data/local/entity/Location.kt
+++ app/src/main/java/com/dylankim/inventoryscanner/data/local/entity/Location.kt
@@ -4,19 +4,20 @@ import androidx.room3.Entity
 import androidx.room3.ForeignKey
 import androidx.room3.PrimaryKey
 
-@Entity("location",
-        foreignKeys = [
-            ForeignKey(
-                entity = Company::class,
-                parentColumns = ["id"],
-                childColumns = ["companyId"]
-            )
-        ]
+@Entity(
+    "location",
+    foreignKeys = [
+        ForeignKey(
+            entity = Company::class,
+            parentColumns = ["id"],
+            childColumns = ["companyId"]
+        )
+    ]
 )
 data class Location(
     @PrimaryKey(autoGenerate = true)
-    val id:Long = 0,
+    val id: Long = 0,
     val companyId: Long,
-    val name:String
+    val name: String
 
 )
