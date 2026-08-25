f33439f24d4e4226f856ed7be68d6bbb473ef88c
Dylan Kim <dylan2.1kim@gmail.com>
2026-08-25 12:50:08 +0900

    feat: initialize Room database

--- app/src/main/java/com/dylankim/inventoryscanner/MainActivity.kt
+++ app/src/main/java/com/dylankim/inventoryscanner/MainActivity.kt
@@ -11,11 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.dylankim.inventoryscanner.data.local.InventoryDatabase
 import com.dylankim.inventoryscanner.ui.theme.InventoryScannerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val database = InventoryDatabase.getDatabase(applicationContext)
+
         enableEdgeToEdge()
         setContent {
             InventoryScannerTheme {
