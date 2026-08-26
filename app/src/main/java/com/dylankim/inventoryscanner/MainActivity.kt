f48fece9c56d63bc9218c6e778e21c9cdf1832e0
Dylan Kim <dylan2.1kim@gmail.com>
2026-08-26 16:15:37 +0900

    feat: Compose ViewModel 사용 설정

--- app/src/main/java/com/dylankim/inventoryscanner/MainActivity.kt
+++ app/src/main/java/com/dylankim/inventoryscanner/MainActivity.kt
@@ -11,14 +11,33 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dylankim.inventoryscanner.data.local.InventoryDatabase
+import com.dylankim.inventoryscanner.data.repository.CompanyRepository
+import com.dylankim.inventoryscanner.ui.company.CompanyViewModel
+import com.dylankim.inventoryscanner.ui.company.CompanyViewModelFactory
 import com.dylankim.inventoryscanner.ui.theme.InventoryScannerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val database = InventoryDatabase.getDatabase(applicationContext)
+
+        val companyRepository = CompanyRepository(
+            database.companyDao()
+        )
+
+        val factory = CompanyViewModelFactory(
+            companyRepository
+        )
+
         enableEdgeToEdge()
         setContent {
+            val viewModel: CompanyViewModel = viewModel(
+                factory = factory
+            )
+
             InventoryScannerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
