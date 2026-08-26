f237ba99616e4dc886ca12c41d1efc4bfa1a5cc9
Dylan Kim <dylan2.1kim@gmail.com>
2026-08-26 12:21:26 +0900

    chore: kotlin 컴파일러 세션파일 제외

---
+++ app/src/main/java/com/dylankim/inventoryscanner/data/repository/CompanyRepository.kt
@@ -0,0 +1,9 @@
+package com.dylankim.inventoryscanner.data.repository
+
+import com.dylankim.inventoryscanner.data.local.dao.CompanyDao
+
+class CompanyRepository(
+    private val companyDao: CompanyDao
+) {
+
+}
