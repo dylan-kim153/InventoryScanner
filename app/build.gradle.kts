3c8032938d0316627e126b035f140964aa4d0e9d
Dylan Kim <dylan2.1kim@gmail.com>
2026-09-15 16:40:43 +0900

    chore: Hilt 기본 환경 및 Application 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
@@ -62,6 +63,10 @@ dependencies {
 
     // ML Kit
     implementation(libs.mlkit.barcode.scanning)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
 
     implementation(libs.androidx.compose.material.icons.extended)
 
---
+++ app/src/main/java/com/dylankim/inventoryscanner/InventoryScannerApplication.kt
@@ -0,0 +1,7 @@
+package com.dylankim.inventoryscanner
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class InventoryScannerApplication : Application()
